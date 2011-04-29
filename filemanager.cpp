a6528b6748745add82f56dfd5eb0633733f24c18
Kolja Windeler <Kolja.Windeler@gmail.com>
2011-04-29 09:25:19 +0200

    hm

--- filemanager.cpp
+++ filemanager.cpp
@@ -1,4 +1,4 @@
-warum geht das denn nicht, he?
+warum geht das denn nicht, he? und nu?
 #define dbug false
 #define BAUDRATE B57600 /* baudrate settings are defined in <asm/termbits.h>, which is included by <termios.h> */
 #define CONNECT_TIME 0000000
